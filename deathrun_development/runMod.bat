b8f946ed02658a9865f53c1e4a09d9dba67e751c
Joelrau <Joelrau@users.noreply.github.com>
2021-02-21 00:03:08 +0200

    Update runMod.bat

--- deathrun_development/runMod.bat
+++ deathrun_development/runMod.bat
@@ -4,4 +4,4 @@ set "map=mp_dr_mirrors_edge"
 set /p "map=Enter map name or press [ENTER] for default [%map%]: "
 
 cd ..\..\
-start iw4x.exe +set fs_game "mods\deathrun_development" +set developer 2 +set developer_script 1 +set logfile 2 +connect localhost +devmap %map%
+start iw4x.exe +set fs_game "mods\deathrun_development" +connect localhost +devmap %map%
